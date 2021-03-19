c60707603722f31897135fb64615cd2541295190
Ionas <seijod17@htl-kaindorf.at>
2021-03-19 18:28:41 +0100

    Trying to deploy

--- seidlserver_main_backend/src/main/java/com/seidlserver/controller/WOLController.java
+++ seidlserver_main_backend/src/main/java/com/seidlserver/controller/WOLController.java
@@ -1,4 +1,23 @@
 package com.seidlserver.controller;
 
+import com.seidlserver.wol.WakeOnLan;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/*
+    Created by: Jonas Seidl
+    Date: 19.03.2021
+    Time: 17:51
+*/
+@RestController
 public class WOLController {
+    @PostMapping("/wake")
+    public String wake(){
+        try{
+            WakeOnLan.wake();
+            return "Magic Packet sent";
+        }catch(Exception ex){
+            return ex.toString();
+        }
+    }
 }
--- seidlserver_main_backend/src/main/java/com/seidlserver/wol/WakeOnLan.java
+++ seidlserver_main_backend/src/main/java/com/seidlserver/wol/WakeOnLan.java
@@ -1,4 +1,54 @@
 package com.seidlserver.wol;
 
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
+import java.net.InetAddress;
+
+
+/*
+    Created by: Jonas Seidl
+    Date: 19.03.2021
+    Time: 17:45
+*/
 public class WakeOnLan {
+
+    public static final int PORT = 9;
+    public static final String ipStr = "10.0.0.255";
+    public static final String macStr = "FC-AA-14-1F-9F-AB";
+
+
+    public static void wake() throws Exception{
+        byte[] macBytes = getMacBytes(macStr);
+        byte[] bytes = new byte[6 + 16 * macBytes.length];
+        for (int i = 0; i < 6; i++) {
+            bytes[i] = (byte) 0xff;
+        }
+        for (int i = 6; i < bytes.length; i += macBytes.length) {
+            System.arraycopy(macBytes, 0, bytes, i, macBytes.length);
+        }
+
+        InetAddress address = InetAddress.getByName(ipStr);
+        DatagramPacket packet = new DatagramPacket(bytes, bytes.length, address, PORT);
+        DatagramSocket socket = new DatagramSocket();
+        socket.send(packet);
+        socket.close();
+    }
+
+    private static byte[] getMacBytes(String macStr) throws IllegalArgumentException {
+        byte[] bytes = new byte[6];
+        String[] hex = macStr.split("(\\:|\\-)");
+        if (hex.length != 6) {
+            throw new IllegalArgumentException("Invalid MAC address.");
+        }
+        try {
+            for (int i = 0; i < 6; i++) {
+                bytes[i] = (byte) Integer.parseInt(hex[i], 16);
+            }
+        }
+        catch (NumberFormatException e) {
+            throw new IllegalArgumentException("Invalid hex digit in MAC address.");
+        }
+        return bytes;
+    }
+
 }
