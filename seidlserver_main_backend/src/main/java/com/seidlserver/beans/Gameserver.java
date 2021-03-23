58ea8660d1ba457b044397fe91259ec029a71900
Jonas <seijod17@htl-kaindorf.at>
2021-03-23 20:03:34 +0100

    CORS fix

---
+++ seidlserver_main_backend/src/main/java/com/seidlserver/beans/Gameserver.java
@@ -0,0 +1,8 @@
+package com.seidlserver.beans;
+/*
+    Created by: Jonas Seidl
+    Date: 23.03.2021
+    Time: 10:48
+*/
+public class Gameserver {
+}
---
+++ seidlserver_main_backend/src/main/java/com/seidlserver/controller/GameserverController.java
@@ -0,0 +1,8 @@
+package com.seidlserver.controller;
+/*
+    Created by: Jonas Seidl
+    Date: 23.03.2021
+    Time: 10:46
+*/
+public class GameserverController {
+}
