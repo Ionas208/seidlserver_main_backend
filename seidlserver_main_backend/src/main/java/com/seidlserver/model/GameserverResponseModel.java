f270cfede1b5cda28e6b86b1af7b74a310fc8b2a
Ionas <seijod17@htl-kaindorf.at>
2021-06-04 12:38:43 +0200

    Fixed login bug

---
+++ seidlserver_main_backend/src/main/java/com/seidlserver/model/GameserverResponseModel.java
@@ -0,0 +1,28 @@
+package com.seidlserver.model;
+
+import com.seidlserver.pojos.gameserver.GameserverType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+/*
+    Created by: Jonas Seidl
+    Date: 04.06.2021
+    Time: 12:35
+*/
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class GameserverResponseModel {
+    public int id;
+
+    public String script;
+
+    public String servername;
+
+    private GameserverType type;
+
+    private String owner;
+}
